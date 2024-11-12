a5485be12e0bbda269b29287a04ea7f6c7207d2a
Kirill_Lekhov <k.lekhov@comearth.ru>
2024-11-12 14:49:34 +0300

    Improve clean_requirements function

--- pip_audit_extra/requirement.py
+++ pip_audit_extra/requirement.py
@@ -1,18 +1,19 @@
 from typing import Final
 
 
-HASH_PREFIX: Final[str] = "--hash"
+PREFIX_HASH: Final[str] = "--hash"
+PREFIX_COMMENT: Final[str] = "#"
 
 
 def clean_requirements(requirements: str) -> str:
 	"""
-	Cleans project requirements.txt file content, removes hashes and python versions.
+	Cleans project requirements.txt file content, removes hashes, comments and python versions.
 	"""
 	lines = requirements.split("\n")
 	dependencies = []
 
 	for line in lines:
-		if line.lstrip().startswith(HASH_PREFIX):
+		if line.lstrip().startswith((PREFIX_HASH, PREFIX_COMMENT)):
 			continue
 
 		line_parts = line.split(" ; ")
